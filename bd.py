a43314f504b74bbf70bfe73207523e201a8aded1
ppbarros <ppbarros@sempreceub.com>
2019-04-05 22:52:25 -0300

    Prova - LTP2 - Pedro Paulo

---
+++ bd.py
@@ -0,0 +1,36 @@
+def get_professores(cursor):
+    cursor.execute(f'select nome, idprofessor from professor order by nome asc')
+    professores = cursor.fetchall()
+    cursor.close()
+    return professores
+
+
+def exibir_professor(cursor, id):
+    cursor.execute(f'select nome, datanasc, nomemae, titulacao, idprofessor from professor where idprofessor = {id}')
+    professor = cursor.fetchone()
+    return professor
+
+def exibir_disciplina(cursor, id):
+    cursor.execute(f'select nome, curso, cargahoraria from disciplina where idprofessor = {id}')
+    disciplinas = cursor.fetchall()
+    cursor.close()
+    return disciplinas
+
+def titulacao(cursor, tit):
+    cursor.execute(f'select nome, idprofessor from professor where titulacao = {tit} order by nome asc')
+    professores = cursor.fetchall()
+    cursor.close()
+    return professores
+
+#def cic(cursor):
+
+def salario(cursor, id):
+    cursor.execute(f'select sum(cargahoraria) from disciplina where idprofessor = {id}')
+    carga = cursor.fetchone()
+    cursor.close()
+    return carga
+
+def nome(cursor, id):
+    cursor.execute(f'select nome from professor where idprofessor = {id}')
+    professor = cursor.fetchone()
+    return professor
---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
+from bd import *
+
+app = Flask(__name__)
+mysql = MySQL()
+mysql.init_app(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'faculdade'
+
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route('/listarProfessores/')
+def professores():
+    cursor = mysql.get_db().cursor()
+    return render_template('professores.html', professores=get_professores(cursor))
+
+@app.route('/exibirProfessor/<id>')
+def mostrar_professor(id):
+    cursor = mysql.get_db().cursor()
+    return render_template('detalhe.html', detalhe=exibir_professor(cursor, id), disciplina=exibir_disciplina(cursor, id))
+
+@app.route('/consultarPorTitulacao/<tit>')
+def tit(tit):
+    cursor = mysql.get_db().cursor()
+    return render_template('titulacao.html', titulacao=tit, data=titulacao(cursor, tit))
+
+@app.route('/calcularSalarioProfessor/<id>')
+def consultar_salario(id):
+    cursor = mysql.get_db().cursor()
+    return render_template('salario.html', professor=nome(cursor, id), carga=salario(cursor, id))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
